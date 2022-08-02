9be4d06650cda0668d475e0eeb0371b1424f51d1
acmpo6ou <acmpo6ou@gmail.com>
2022-08-02 20:48:19 +0200

    🌟 Create MainWindow

--- core/main_window.py
+++ core/main_window.py
@@ -1,49 +1,29 @@
-#!/usr/bin/python3.9
-
 #   Copyright (c) 2022. Bohdan Kolvakh
 #   This file is part of Synastry.
-#
+#  #
 #   Synastry is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
-#
+#  #
 #   Synastry is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
-#
+#  #
 #   You should have received a copy of the GNU General Public License
 #   along with Synastry.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-import gi
+from gi.repository import Gtk
 
-gi.require_version("Gtk", "3.0")
-gi.require_version("Gdk", "3.0")
-from gi.repository import Gtk, Gio
-
-import sys
-
-from core.main_window import MainWindow
+from core.gtk_utils import GladeTemplate
 
 
-class Application(Gtk.Application):
+class MainWindow(Gtk.ApplicationWindow, GladeTemplate):
     def __init__(self, *args, **kwargs):
-        super().__init__(
-            *args,
-            application_id="com.acmpo6ou.Synastry",
-            flags=Gio.ApplicationFlags.HANDLES_OPEN,
-            **kwargs
-        )
-        self.window = None
+        Gtk.ApplicationWindow.__init__(self, *args, **kwargs)
+        GladeTemplate.__init__(self, "main_window")
 
-    def do_activate(self):
-        if not self.window:
-            self.window = MainWindow(application=self, title="Synastry")
-            self.window.show_all()
-
-
-if __name__ == "__main__":
-    app = Application()
-    app.run(sys.argv)
+        self.set_default_size(1280, 720)
+        # self.set_icon_from_file("img/icon.svg")
