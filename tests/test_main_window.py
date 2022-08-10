88e7a80130fd314003d6408762cdf46ef3710349
acmpo6ou <acmpo6ou@gmail.com>
2022-08-10 09:56:47 +0200

    🎨 Test color of aspect labels

--- tests/test_main_window.py
+++ tests/test_main_window.py
@@ -14,12 +14,12 @@
 #   You should have received a copy of the GNU General Public License
 #   along with Synastry.  If not, see <https://www.gnu.org/licenses/>.
 #
-import itertools
 
-from core.main_window import MainWindow
+from core.main_window import MainWindow, RED, GREEN
 
 DATE = "2022-08-04 12:00"
 DATE2 = "2022-08-06 12:00"
+NO_COLOR = "<span >"
 
 
 def test_calculate_conflictedness():
@@ -27,8 +27,14 @@ def test_calculate_conflictedness():
     grid = window.conflicts1
     conflictedness = window.calculate_conflictedness(grid, DATE)
 
-    assert grid.get_child_at(2, 1).text == "87.6°"
+    label = grid.get_child_at(2, 1)
+    assert label.text == "87.6°"
+    assert RED in label.markup
     assert conflictedness == ["mars", "saturn"]
+
+    no_color = len([label for label in grid.children
+                    if NO_COLOR in label.markup])
+    assert no_color == 5
 
 
 def test_calculate_conflicts():
@@ -37,32 +43,81 @@ def test_calculate_conflicts():
     conf2 = window.calculate_conflictedness(window.conflicts2, DATE2)
     window.calculate_conflicts(DATE, DATE2, conf1, conf2)
 
-    for i in range(1, 5):
-        degrees = window.conflicts.get_child_at(i, i).text
-        assert degrees in ("1.3°", "0.1°", "0.0°")
+    label = window.conflicts.get_child_at(1, 1)
+    assert label.text == "1.3°"
+    assert "red" in label.markup
 
-    assert window.conflicts.get_child_at(3, 1).text == "87.8°"
-    assert window.conflicts.get_child_at(1, 3).text == "88.9°"
+    label = window.conflicts.get_child_at(2, 2)
+    assert label.text == "0.1°"
+    assert NO_COLOR in label.markup
+
+    label = window.conflicts.get_child_at(3, 3)
+    assert label.text == "0.1°"
+    assert "red" in label.markup
+
+    label = window.conflicts.get_child_at(4, 4)
+    assert label.text == "0.0°"
+    assert RED in label.markup
+
+    label = window.conflicts.get_child_at(3, 1)
+    assert label.text == "87.8°"
+    assert "red" in label.markup
+
+    label = window.conflicts.get_child_at(1, 3)
+    assert label.text == "88.9°"
+    assert "red" in label.markup
+
+    no_color = len([label for label in window.conflicts.children
+                    if NO_COLOR in label.markup])
+    assert no_color == 11
 
 
 def test_calculate_love():
     window = MainWindow()
     window.calculate_love(DATE, DATE2)
-    assert window.love.get_child_at(2, 2).text == "59.6°"
+
+    label = window.love.get_child_at(2, 2)
+    assert label.text == "59.6°"
+    assert GREEN in label.markup
+
+    no_color = len([label for label in window.love.children
+                    if NO_COLOR in label.markup])
+    assert no_color == 3
 
 
 def test_calculate_friendship():
     window = MainWindow()
     window.calculate_friendship(DATE, DATE2)
 
-    assert window.friendship.get_child_at(1, 1).text == "1.9°"
-    assert window.friendship.get_child_at(3, 3).text == "2.4°"
-    assert window.friendship.get_child_at(2, 3).text == "126.9°"
+    label = window.friendship.get_child_at(1, 1)
+    assert label.text == "1.9°"
+    assert GREEN in label.markup
+
+    label = window.friendship.get_child_at(3, 3)
+    assert label.text == "2.4°"
+    assert GREEN in label.markup
+
+    label = window.friendship.get_child_at(2, 3)
+    assert label.text == "126.9°"
+    assert GREEN in label.markup
+
+    no_color = len([label for label in window.friendship.children
+                    if NO_COLOR in label.markup])
+    assert no_color == 6
 
 
 def test_calculate_happiness():
     window = MainWindow()
     window.calculate_happiness(window.happiness2, DATE2, DATE)
 
-    assert window.happiness2.get_child_at(1, 1).text == "125.4°"
-    assert window.happiness2.get_child_at(2, 2).text == "84.6°"
+    label = window.happiness2.get_child_at(1, 1)
+    assert label.text == "125.4°"
+    assert GREEN in label.markup
+
+    label = window.happiness2.get_child_at(2, 2)
+    assert label.text == "84.6°"
+    assert RED in label.markup
+
+    no_color = len([label for label in window.happiness2.children
+                    if NO_COLOR in label.markup])
+    assert no_color == 2
